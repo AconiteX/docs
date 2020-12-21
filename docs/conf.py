d710cbb138399ef8fc9a5238e9cce73438a0ea00
AconiteGodOfSWG <63141077+AconiteGodOfSWG@users.noreply.github.com>
2020-12-20 19:39:25 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,5 @@
-# see: https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Sphynx Config Info: https://www.sphinx-doc.org/en/master/usage/configuration.html
+# Theme Config Info: https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
 
 project = 'SWG Source'
 copyright = '2020, SWG Source'
@@ -6,12 +7,16 @@ author = 'SWG Source'
 
 extensions = [
 ]
-
 templates_path = ['_templates']
-
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = ["_themes", ]
-
+html_theme_options = {
+  'logo_only': True,
+  'display_version': False,
+  'style_external_links': True,
+  'includehidden': False,
+  'titles_only': True,
+}
 html_static_path = ['_static']
